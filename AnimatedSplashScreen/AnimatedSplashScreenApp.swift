fee0bbcd60de7b0ae390ebc2a8700447f75f761b
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-19 22:21:00 +0200

    Initial Commit

---
+++ AnimatedSplashScreen/AnimatedSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSplashScreenApp.swift
+//  AnimatedSplashScreen
+//
+//  Created by Michele Manniello on 19/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedSplashScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedSplashScreen
+//
+//  Created by Michele Manniello on 19/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
